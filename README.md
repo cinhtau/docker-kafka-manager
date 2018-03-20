32dddca906b59d4c843553e1506eeb7fd0ea7d2f
Nguyen, Tan Quang Vinh <TanQuangVinh.Nguyen@six-group.com>
2018-03-20 11:28:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Docker Image for [Kafka Manager by Yahoo](https://github.com/yahoo/kafka-manager).
+
+[License](LICENSE) is derivative from the Yahoo project: `APL v2.0`.
+
---
+++ start-kafka-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# taken from
+# https://github.com/sheepkiller/kafka-manager-docker/blob/master/start-kafka-manager.sh
+
+if [[ $KM_USERNAME != ''  && $KM_PASSWORD != '' ]]; then
+    sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo 'basicAuthentication.enabled=true' >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo "basicAuthentication.username=${KM_USERNAME}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo "basicAuthentication.password=${KM_PASSWORD}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo 'basicAuthentication.realm="Kafka-Manager"' >> /kafka-manager-${KM_VERSION}/conf/application.conf
+fi
+
+exec ./bin/kafka-manager -Dconfig.file=${KM_CONFIGFILE} "${KM_ARGS}" "${@}"
